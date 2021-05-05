2cc6901720706406a4673aadd66bf2dae40f3aaf
amirhoseinRj <crowdfund3>
2021-05-05 13:31:25 +0430

    move_forward and move_backward added and  check_next_move updated

--- AdditionalFunctions.py
+++ AdditionalFunctions.py
@@ -1,7 +1,4 @@
-# it takes the enviroment, our next move and robot coordinates as input and checks whether the robot can make this move or not
-def check_next_move(enviroment, next_move, robot_coordinates):
-
-    num_rows, num_cols = len(enviroment), len(enviroment[0])
+def move_forward(next_move, robot_coordinates):
 
     if next_move == 'u':
         robot_coordinates['y'] += 1
@@ -15,9 +12,35 @@ def check_next_move(enviroment, next_move, robot_coordinates):
     elif next_move == 'l':
         robot_coordinates['x'] -= 1
 
+    return robot_coordinates
+
+def move_backward(next_move, robot_coordinates):
+
+    if next_move == 'u':
+        robot_coordinates['y'] -= 1
+
+    elif next_move == 'd':
+        robot_coordinates['y'] += 1
+
+    elif next_move == 'r':
+        robot_coordinates['x'] -= 1
+
+    elif next_move == 'l':
+        robot_coordinates['x'] += 1
+
+    return robot_coordinates
+
+
+# it takes the enviroment, our next move and robot coordinates as input and checks whether the robot can make this move or not
+def check_next_move(enviroment, next_move, robot_coordinates):
+
+    num_rows, num_cols = len(enviroment), len(enviroment[0])
+    robot_coordinates = move_forward(next_move, robot_coordinates)
+
     # robot is not allowed to go outside the enviroment
-    if True:
-        pass
+    if robot_coordinates.get('x') > num_rows or robot_coordinates.get('y') > num_cols:
+        robot_coordinates = move_backward(next_move, robot_coordinates)
+        return False
 
     # robot is not allowed to go to the cells with x in it
     elif True:
