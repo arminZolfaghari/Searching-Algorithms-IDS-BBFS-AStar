6d7b2ece02ad36c0670602c5b73fa61499a5255b
unknown <arminzd1@gmail.com>
2021-05-06 07:04:04 +0430

    create child and frontier in IDS.py and write some functions in additionalFunctions.py

--- AdditionalFunctions.py
+++ AdditionalFunctions.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 # read file and return information about environment
 def read_file(file_name):
     path = "./input/" + file_name
@@ -19,7 +22,7 @@ def read_file(file_name):
                 row_matrix_with_cost.append(row[j])
 
                 if row[j].isnumeric():
-                    row_matrix_without_cost.append("1")
+                    row_matrix_without_cost.append("")
                     row_matrix_cost.append(row[j])
                 else:
                     if row[j] == "x":
@@ -46,22 +49,25 @@ def move_forward(environment, next_move, robot_coordinates):
     num_rows, num_cols = len(environment), len(environment[0])
 
     if next_move == 'u':
-        robot_coordinates['y'] -= 1
-
-    elif next_move == 'd':
-        robot_coordinates['y'] += 1
-
-    elif next_move == 'r':
-        robot_coordinates['x'] += 1
-
-    elif next_move == 'l':
         robot_coordinates['x'] -= 1
 
+    elif next_move == 'd':
+        robot_coordinates['x'] += 1
+
+    elif next_move == 'r':
+        robot_coordinates['y'] += 1
+
+    elif next_move == 'l':
+        robot_coordinates['y'] -= 1
+
+    robot_new_coordinates = {"x": robot_coordinates['x'], "y": robot_coordinates["y"]}
     # robot is not allowed to go outside the environment
-    if 0 <= robot_coordinates['x'] <= num_rows and 0 <= robot_coordinates['y'] <= num_cols:
-        return True, robot_coordinates
+    if 0 <= robot_new_coordinates['x'] < num_rows and 0 <= robot_new_coordinates['y'] < num_cols:
+        print(next_move)
+        print(robot_new_coordinates)
+        return True, robot_new_coordinates
     else:
-        return False, robot_coordinates
+        return False, robot_new_coordinates
 
 
 # def move_backward(next_move, robot_coordinates):
@@ -89,20 +95,23 @@ def check_next_move(environment, next_move, robot_coordinates):
     if not is_first_step_available:
         return False
 
-    # robot is not allowed to go to the cells with x or p in it
-    elif ('x' or 'p') in environment[robot_next_coordinates.get('x')][robot_next_coordinates.get('y')] or 'p' :
-        return False
-
-    # robot can't push two cells both with butter or after butter cell, cell is x
-    is_two_step_available, robot_2next_coordinates = move_forward(environment, next_move, robot_next_coordinates)
-    if is_two_step_available:
-        if 'b' in environment[robot_next_coordinates['x']][robot_next_coordinates['y']] and 'b' in environment[robot_2next_coordinates['x'][robot_2next_coordinates['y']]]:
-            return False
-        if 'b' in environment[robot_next_coordinates['x']][robot_next_coordinates['y']] and 'x' in environment[robot_2next_coordinates['x'][robot_2next_coordinates['y']]]:
-            return False
-
     else:
-        return True
+        # robot is not allowed to go to the cells with x or p in it
+        if ('x' or 'p' or 'rp') in environment[robot_next_coordinates.get('x')][robot_next_coordinates.get('y')]:
+            return False
+
+        # robot can't push two cells both with butter or after butter cell, cell is x
+        is_two_step_available, robot_2next_coordinates = move_forward(environment, next_move, robot_next_coordinates)
+        if is_two_step_available:
+            if 'b' in environment[robot_next_coordinates['x']][robot_next_coordinates['y']] and 'b' in environment[
+                robot_2next_coordinates['x']][robot_2next_coordinates['y']]:
+                return False
+            if 'b' in environment[robot_next_coordinates['x']][robot_next_coordinates['y']] and 'x' in environment[
+                robot_2next_coordinates['x'][robot_2next_coordinates['y']]]:
+                return False
+
+        else:
+            return True
 
 
 # get all permitted movements in each state
@@ -114,3 +123,110 @@ def get_all_permitted_movements(environment, robot_coordinates):
             all_permitted_movements.append(movement)
 
     return all_permitted_movements
+
+
+movement_to_coordinate = {'u': {"x": -1}, 'r': {"y": +1}, 'd': {"x": +1}, 'l': {"y": -1}}
+
+
+def dsum(*dicts):
+    ret = defaultdict(int)
+    for d in dicts:
+        for k, v in d.items():
+            ret[k] += v
+    return dict(ret)
+
+
+# get environment and movement then return new environment and new robot coordinates
+def update_environment(environment, current_robot_coordinates, movement):
+    new_robot_coordinates = dsum(current_robot_coordinates, movement_to_coordinate[movement])
+    curr_robot_x_coordinate, curr_robot_y_coordinate = current_robot_coordinates['x'], current_robot_coordinates['y']
+    new_robot_x_coordinate, new_robot_y_coordinate = new_robot_coordinates['x'], new_robot_coordinates['y']
+
+    # next robot coordinates have butter
+    if environment[new_robot_x_coordinate][new_robot_y_coordinate] == 'b':
+        new_butter_coordinates = dsum(new_robot_coordinates, movement_to_coordinate[movement])
+        new_butter_x_coordinate, new_butter_y_coordinate = new_butter_coordinates['x'], new_butter_coordinates['y']
+
+        # next butter coordinates have plate
+        if environment[new_butter_x_coordinate][new_butter_y_coordinate] == 'p':
+            environment[new_butter_x_coordinate][new_butter_y_coordinate] = 'bp'
+        else:
+            environment[new_butter_x_coordinate][new_butter_y_coordinate] = 'b'
+
+    environment[curr_robot_x_coordinate][curr_robot_y_coordinate] = ''
+    environment[new_robot_x_coordinate][new_robot_y_coordinate] = 'r'
+
+    return environment, new_robot_coordinates
+
+
+def find_plates_coordinates():
+    environment_with_cost, environment_without_cost, environment_cost, number_of_butters, robot_coordinates = read_file(
+        "test1.txt")
+    num_rows, num_cols = len(environment_without_cost), len(environment_without_cost[0])
+
+    plates_coordinates = []
+    for i in range(num_rows):
+        for j in range(num_cols):
+            if environment_without_cost[i][j] == 'p':
+                plates_coordinates.append({"x": i, "y": j})
+
+    return plates_coordinates
+
+
+def find_butters_coordinates():
+    environment_with_cost, environment_without_cost, environment_cost, number_of_butters, robot_coordinates = read_file(
+        "test1.txt")
+    num_rows, num_cols = len(environment_without_cost), len(environment_without_cost[0])
+
+    plates_coordinates = []
+    for i in range(num_rows):
+        for j in range(num_cols):
+            if environment_without_cost[i][j] == 'b':
+                plates_coordinates.append({"x": i, "y": j})
+
+    return plates_coordinates
+
+
+def generate_all_goal_environment():
+    environment_with_cost, environment_without_cost, environment_cost, number_of_butters, robot_coordinates = read_file(
+        "test1.txt")
+    print(environment_without_cost)
+    start_robot_x_coordinates, start_robot_y_coordinates = robot_coordinates['x'], robot_coordinates['y']
+    all_plates_coordinates = find_plates_coordinates()
+    print(all_plates_coordinates)
+    all_butters_coordinates = find_butters_coordinates()
+    print(all_butters_coordinates)
+
+    for plate_coordinates in all_plates_coordinates:
+        plate_x_coordinate, plate_y_coordinate = plate_coordinates['x'], plate_coordinates['y']
+        environment_without_cost[plate_x_coordinate][plate_y_coordinate] = "bp"
+
+    for butter_coordinates in all_butters_coordinates:
+        butter_x_coordinate, butter_y_coordinate = butter_coordinates['x'], butter_coordinates['y']
+        environment_without_cost[butter_x_coordinate][butter_y_coordinate] = ''
+
+    all_final_robot_coordinates = []
+    for plate_coordinates in all_plates_coordinates:
+        plate_x_coordinate, plate_y_coordinate = plate_coordinates['x'], plate_coordinates['y']
+        all_permitted_movements = get_all_permitted_movements(environment_without_cost, plate_coordinates)
+        print(all_permitted_movements)
+        for movement in all_permitted_movements:
+            final_robot_coordinates = dsum(plate_coordinates, movement_to_coordinate[movement])
+            all_final_robot_coordinates.append(final_robot_coordinates)
+
+    environment_without_cost[start_robot_x_coordinates][start_robot_y_coordinates] = ""
+    all_goal_environment = []
+    for final_robot_coordinates in all_final_robot_coordinates:
+        goal_environment = environment_without_cost
+        final_robot_x_coordinates, final_robot_y_coordinates = final_robot_coordinates['x'], final_robot_coordinates[
+            'y']
+        print(final_robot_coordinates)
+        goal_environment[final_robot_x_coordinates][final_robot_y_coordinates] = 'r'
+        all_goal_environment.append(goal_environment)
+
+    return all_goal_environment
+
+
+if __name__ == "__main__":
+    arr = generate_all_goal_environment()
+    print(arr)
--- IDS.py
+++ IDS.py
@@ -1,4 +1,26 @@
 from collections import defaultdict
+from AdditionalFunctions import *
+
+
+def create_child(node, frontier):
+    curr_environment, curr_robot_coordinates, curr_depth = node.environment, node.robot_coordinates, node.depth
+    all_permitted_movements = get_all_permitted_movements(curr_environment, curr_robot_coordinates)
+    print(111111111)
+    print(all_permitted_movements)
+    for movement in all_permitted_movements:
+        new_environment, new_robot_coordinates = update_environment(curr_environment, curr_robot_coordinates, movement)
+        child_node = Node(new_environment, new_robot_coordinates, curr_depth + 1, movement)
+        frontier.insert(0, child_node)
+
+    return frontier
+
+
+class Node:
+    def __init__(self, environment, robot_coordinates, depth, movement):
+        self.environment = environment
+        self.robot_coordinates = robot_coordinates
+        self.depth = depth
+        self.movement = movement
 
 
 class Graph:
@@ -31,17 +53,25 @@ class Graph:
 
 
 if __name__ == "__main__":
+    environment_with_cost, environment_without_cost, environment_cost, number_of_butters, robot_coordinates = read_file(
+        "test1.txt")
+    print(environment_without_cost)
+    start_node = Node(environment_without_cost, robot_coordinates, 0, " ")
+    frontier = []
+    new_frontier = create_child(start_node, frontier)
+    print(new_frontier)
 
-    g = Graph(7)
-    g.addEdge(0, 1)
-    g.addEdge(0, 2)
-    g.addEdge(1, 3)
-    g.addEdge(1, 4)
-    g.addEdge(2, 5)
-    g.addEdge(2, 6)
 
-    target = 6
-    maxDepth = 3
-    src = 0
-
-    print(g.IDS(src, target, maxDepth))
+    # g = Graph(7)
+    # g.addEdge(0, 1)
+    # g.addEdge(0, 2)
+    # g.addEdge(1, 3)
+    # g.addEdge(1, 4)
+    # g.addEdge(2, 5)
+    # g.addEdge(2, 6)
+    #
+    # target = 6
+    # maxDepth = 3
+    # src = 0
+    #
+    # print(g.IDS(src, target, maxDepth))
