0eab80e6aea0abb5e12be052103207a63d2b87cb
unknown <arminzd1@gmail.com>
2021-05-10 15:27:08 +0430

    complete generate_node function and functions about frontier, explored

--- A-Star-Algorithm.py
+++ A-Star-Algorithm.py
@@ -68,14 +68,73 @@ def calculate_heuristic_environment(plates_coordinates_arr, environment_without_
     return heuristic_environment
 
 
+def get_heuristic_point(point):
+    return
+
+
 # calculate_heuristic 2
 # def calculate_heuristic(point, butters_coordinates_arr, plates_coordinates_arr):
 #     butters_arr_sorted, plates_arr_sorted = find_butter_for_plate(butters_coordinates_arr, plates_coordinates_arr)
 #
 
 
+# sort frontier list by cost nodes
+def sort_frontier_by_cost(frontier):
+    frontier.sort(key=lambda x: x.cost, reverse=False)
+
+
+# return cost of point
+def calculate_cost_f_node(new_cost_g, new_robot_coordinates, huerisitic_arr):
+    return new_cost_g + huerisitic_arr[new_robot_coordinates['x']][new_robot_coordinates['y']]
+
+
+def calculate_cost_g_node(environment_cost, new_robot_coordinates, curr_cost_g):
+    return curr_cost_g + environment_cost[new_robot_coordinates['x']][new_robot_coordinates['y']]
+
+
+def is_new_node_in_frontier(frontier, new_node):
+    for node in frontier:
+        if node.environment == new_node.environment:
+            return True, node
+    return False, new_node
+
+
+def is_new_node_in_explored(explored, new_node):
+    for node in explored:
+        if node.environment == new_node.environment:
+            return True
+    return False
+
+
+def update_frontier_explored(frontier, explored, new_node):
+    if not is_new_node_in_explored(explored, new_node):
+        is_duplicate, duplicate_node = is_new_node_in_frontier(frontier, new_node)
+        if is_duplicate:
+            if new_node.cost_f < duplicate_node.cost_f:
+                frontier.remove(duplicate_node)
+                frontier.append(new_node)
+        else:
+            frontier.append(new_node)
+
+    sort_frontier_by_cost(frontier)
+
+
+def generate_node(node, envrironment_cost, hueristic_arr, frontier, explored):
+    curr_environment, curr_robot_coordinates, curr_depth = node.environment, node.robot_coordinates, node.depth
+    curr_cost_g, curr_cost_f = node.cost_g, node.cost_f
+    all_permitted_movements = get_all_permitted_movements(curr_environment, curr_robot_coordinates)
+    for movement in all_permitted_movements:
+        new_environment, new_robot_coordinates = update_environment(curr_environment, curr_robot_coordinates, movement)
+        new_cost_g = calculate_cost_g_node(envrironment_cost, new_robot_coordinates, curr_cost_g)
+        new_cost_f = calculate_cost_f_node(new_cost_g, new_robot_coordinates, hueristic_arr)
+        new_node = Node(new_environment, new_robot_coordinates, curr_depth + 1, movement, node, new_cost_g, new_cost_f)
+        update_frontier_explored(frontier, explored, new_node)
+
+
+
 if __name__ == "__main__":
-    environment_without_cost = read_file("test3.txt")[1]
+    environment_without_cost, environment_cost, robot_coordinates = read_file("test3.txt")[1], read_file("test3.txt")[
+        2], read_file("test3.txt")[4]
     print(environment_without_cost)
     arr_plates = find_plates_coordinates("test3.txt")
     print(arr_plates)
--- Node.py
+++ Node.py
@@ -1,10 +1,16 @@
 class Node:
-    def __init__(self, environment, robot_coordinates, depth, movement, parent):
+    def __init__(self, environment, robot_coordinates, depth, movement, parent, cost_g, cost_f):
         self.environment = environment
         self.robot_coordinates = robot_coordinates
         self.depth = depth
         self.movement = movement
         self.parent = parent
+        self.cost_g = cost_g
+        self.cost_f = cost_f
+
+    # def __eq__(self, others):
+    #     if self.environment == others.environment
+
 
 class Initial_node:
     def __init__(self, environment):
