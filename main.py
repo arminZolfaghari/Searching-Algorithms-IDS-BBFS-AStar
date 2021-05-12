bfe015884c63903e4498490c7026f29d97e5be93
amirhoseinRj <crowdfund3>
2021-05-12 18:03:38 +0430

    main takes algorithm as input

--- main.py
+++ main.py
@@ -9,43 +9,44 @@ import time
 
 if __name__ == "__main__":
 
-    file_name = 'test4.txt'
+    file_name = 'test1.txt'
 
-    # IDS Algorithm
-    # ids.start_ids_algorithm("test1.txt", 15)
+    algorithm = input("Which Algorithm do you want to use?\n1) IDS\n2) BBFS\n3) A*\n")
 
+    if algorithm == "1" or algorithm == "IDS":
+        # ids.start_ids_algorithm(file_name, 15)
+        pass
 
-    # BBFS ALgorithm
-    start_time = time.time()
-    has_result, path = bbfs.BBFS(file_name)
-    finish_time = time.time()
-    duration = (finish_time - start_time)
-    if not has_result:
-        print('there is no answer in this environment!')
-    else:
-        movement_list = funcs.find_movement_list(path)
-        print('path length is: ', len(path))
-        print('path is: ', movement_list)
-        funcs.print_path(path)
-        g = gui.GraphicalInterface(path)
-        g.Visualize()
-        # funcs.write_to_file("BBFS", file_name, movement_list, duration)
+    elif algorithm == "2" or algorithm == "BBFS":
+        start_time = time.time()
+        has_result, path = bbfs.BBFS(file_name)
+        finish_time = time.time()
+        duration = (finish_time - start_time)
+        if not has_result:
+            print('there is no answer in this environment!')
+        else:
+            movement_list = funcs.find_movement_list(path)
+            print('path length is: ', len(path))
+            print('path is: ', movement_list)
+            funcs.print_path(path)
+            g = gui.GraphicalInterface(path)
+            g.Visualize()
+            # funcs.write_to_file("BBFS", file_name, movement_list, duration)
 
-
-    # A* Algorithm
-    # start_time = time.time()
-    # has_result, path, goal_depth = astar.start_a_star_algorithm(file_name, 35)
-    # finish_time = time.time()
-    # duration = (finish_time - start_time)
-    # if not has_result:
-    #     print('there is no answer in this environment!')
-    # else:
-    #     movement_list = funcs.find_movement_list(path)
-    #     print("path costs : ", path[-1].cost_g)
-    #     print("depth of goal : ", goal_depth)
-    #     funcs.print_path(path)
-        # g = gui.GraphicalInterface(path)
-        # g.Visualize()
-        # funcs.write_to_file("ASTAR", file_name, movement_list, duration)
+    elif algorithm == "3" or algorithm == "A*":
+        start_time = time.time()
+        has_result, path, goal_depth = astar.start_a_star_algorithm(file_name, 35)
+        finish_time = time.time()
+        duration = (finish_time - start_time)
+        if not has_result:
+            print('there is no answer in this environment!')
+        else:
+            movement_list = funcs.find_movement_list(path)
+            print("path costs : ", path[-1].cost_g)
+            print("depth of goal : ", goal_depth)
+            funcs.print_path(path)
+            g = gui.GraphicalInterface(path)
+            g.Visualize()
+            funcs.write_to_file("ASTAR", file_name, movement_list, duration)
 
 
